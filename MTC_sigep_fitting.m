37f9a8cb1b55d3b9fda8f3db1991a5dccb8c5a28
rwiener92 <91078835+rwiener92@users.noreply.github.com>
2022-05-20 19:18:19 -0400

    modifications

--- MTC_sigep_fitting.m
+++ MTC_sigep_fitting.m
@@ -14,7 +14,7 @@ Ringdat_SSsigep_UTSS(end-1:end,:) = []; %interp error, end of broken tissue read
 x = Ringdat_SSsigep_UTSS(:,1);
 v = Ringdat_SSsigep_UTSS(:,2);
 xq = [0:0.05:Ringdat_SSsigep_UTSS(end,1)];
-vq1 = interp1(x,v,xq, 'spline'); %default='linear', also try 'spline' or 'cubic'
+vq1 = interp1(x,v,xq, 'linear'); %default='linear', also try 'spline' or 'cubic'
 %use this to plot original and interpolated data
 figure; plot(x,v,'o',xq,vq1,':.');
 %
@@ -27,21 +27,21 @@ intpdat = horzcat(xq, vq1);
 %%% YOUNGS MODULUS E
 %Linear regression for polynomial fit to calculated data ranges
 %Fit line to data segments
-strain_range = [0, 1.200]; % set strain range (e.g. to 150% strain_range=1.500)
+strain_range = [0.100, 0.500]; % set strain range (e.g. to 150% strain_range=1.500)
 intpdat(:,1) = round(intpdat(:,1),3); %round intpdat(strain) to 3 decimals for integer
 strain_range_index = [ find( intpdat(:,1) == strain_range(1) ), find( intpdat(:,1) == strain_range(2) ) ];
 [c, S] = polyfit(intpdat(strain_range_index(1):strain_range_index(2) ,1), intpdat(strain_range_index(1):strain_range_index(2) ,2), 1); %1=first order (y=mx+b)
-y_est = polyval(c, intpdat(1:strain_range_index,1));%error
+y_est = polyval(c, intpdat(strain_range_index(1):strain_range_index(2), 1));%error
 
 
 % Plot orginal, interpolated, and curve fits
 figure; plot(Ringdat_SSsigep_UTSS(:,1), Ringdat_SSsigep_UTSS(:,2), 'ko','LineWidth',1.5)
-hold on; plot(xq(1:strain_range_index),vq1(1:strain_range_index),'--','LineWidth',3);
-hold on; plot(intpdat(1:strain_range_index,1), y_est,'k','LineWidth',1.5)
+hold on; plot( xq(strain_range_index(1):strain_range_index(2)), vq1(strain_range_index(1):strain_range_index(2)), '--','LineWidth',3);
+hold on; plot( intpdat(strain_range_index(1):strain_range_index(2) ,1), y_est, 'k','LineWidth',1.5)
 ylabel('Stress σ [kPa]');
 xlabel('Strain ε');
 %ylim([0, 10])
-%xlim([0, strain_range])
+%xlim([ ])
 
 
 %%% modify RR_avg fits to include various strain ranges

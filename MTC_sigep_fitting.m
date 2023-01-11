14f7232ebef107d176d7440bdafc5e0516ae9250
rwiener92 <91078835+rwiener92@users.noreply.github.com>
2023-01-10 22:29:47 -0500

    Update MTC_sigep_fitting.m

--- MTC_sigep_fitting.m
+++ MTC_sigep_fitting.m
@@ -6,7 +6,11 @@ function data_cell = MTC_sigep_fitting(Original_UTSS_Data)
 % Stress zero the data
 % Fit a polynomial to the desired strain_range
 % Calculate secant and tangent modulus off polynomial fit at strain_range.
+%
+% NOTE: code is written for second order (quadratic) modeling
+%
 % REQUIRED ADD-ONS: <Symbolic Math Toolbox>
+%
 %
 %   Plotting:
 %       1. Original data (black circle)
@@ -36,7 +40,7 @@ polynomial_order = 2;  % ( 2=second order: y=p(1)x^2+p(2)x+p(3) )
 strain_range = [0.300, 0.650];  % set strain range (e.g. 150% strain_range=1.500) %note this has interpolation_steps
 tangent_point_strain_1 = 0.650; % typically max strain_range (strain_range(2)), but can be e.g. 0.05
 tangent_point_strain_2 = median(strain_range); %try defining as middle of strain_range
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 % Store original data for data_cell
@@ -59,14 +63,14 @@ vq1 = vq1';
 intpdat = horzcat(xq, vq1);
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% FITTING THE STRESS_STRAIN DATA %%%
 %Polynomial fit to calculated data ranges
 %Fit curve to multiple data segments for stiffness interpretation
 intpdat(:,1) = round(intpdat(:,1),3); %round intpdat(strain) to 3 decimals for integer
 strain_range_index = [ find( intpdat(:,1) == strain_range(1) ), find( intpdat(:,1) == strain_range(2) ) ];
 
-%Linear fit "polyfit(~,~,1)" means p(1) is slope/stiffness(m)
+%Quadratic fit "polyfit(~,~,2)" means p(1) is p(1)*x^2
 [p, S] = polyfit(intpdat(strain_range_index(1):strain_range_index(2) ,1), intpdat(strain_range_index(1):strain_range_index(2) ,2), polynomial_order); %2=second order: y=p(1)x^2+p(2)x+p(3)
 [y_est, delta] = polyval(p, intpdat(strain_range_index(1):strain_range_index(2), 1), S); %polyfit y_estimates & CI
 %Calculare R2 error
@@ -78,11 +82,11 @@ R2 = 1 - (SSR/SST);
 [pks, pks_loc] = findpeaks(Ringdat_SSsigep_UTSS(:,2),Ringdat_SSsigep_UTSS(:,1));
 Y_stress = pks(1); %yield stress (kPa)
 Y_strain = pks_loc(1); %yield strain
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% CALCULATING SECANT MODULUS FROM STRAIN_RANGE %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% CALCULATE SECANT MODULUS FROM STRAIN_RANGE %%%
 % Slope of line defined by (y2-y1)/(x2-x1)
 % Use this matrix for plotting
 sec(1,1) = intpdat(1,1); %initial strain (= 0) [sec x1]
@@ -94,14 +98,14 @@ sec(2,2) = y_est(end); %end stress (at end of strain range) of fitted curve [sec
 secant_slope = ( y_est(end) - intpdat(1,2) ) / ( strain_range(2) - intpdat(1,1) ) ;
 
 %%%%%%%%%%%%%%%%
-%%% CALCULATING TANGENT MODULUS FROM STRAIN_RANGE %%%
+%%% CALCULATE TANGENT MODULUS FROM STRAIN_RANGE %%%
 % differnetiate polyfit equation (which is over strain range)
-syms T(x)
+syms Fstrain x
 
 % equation of polyfit over strain range
-T(x) = (p(1))*x.^2 + (p(2))*x + (p(3));
-% differentiate T wrt x (x is strain)
-dT(x) = diff(T,x);  %result should be dT(x)= (p(1))*2*x + (p(2))
+Fstrain = @(x) (p(1))*x.^2 + (p(2))*x + (p(3));
+% differentiate ds wrt x (x is strain)
+ds(x) = diff(Fstrain,x);  %result should be ds(x)= (p(1))*2*x + (p(2))
 
 % find slope of tangent line @ tangent_point
 % manual power law diff of fit (change if higher order poly_fit)
@@ -130,10 +134,31 @@ tang_2(1,1) = strain_range(1);
 tang_2(2,1) = strain_range(2);
 tang_2(1,2) = tangent_slope_2*strain_range(1) + tangent_intercept_2; %y=mx+b
 tang_2(2,2) = tangent_slope_2*strain_range(2) + tangent_intercept_2; %y=mx+b
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% CALCULATE STRAIN ENERGY FROM POLY_FIT ACROSS STRAIN_RANGE %%%
+%syms Fstrain x % redundant this is initialized in TANGENT
+%Fstrain = @(x) (p(1))*x.^2 + (p(2))*x + (p(3));
+
+% Integrate poly_fit function, only over strain_range (offset to account for bending)
+% calculate the numeric integration of input function ‘Fx’, which in turn signifies the area under a curve.
+% A = integral (Fx, Xminimum, Xmaximum)
+AOC_over_strain_range = integral(Fstrain, strain_range(1), strain_range(2)); %for our quadratic this in incorrect because it doesnt fit to zero
+
+% gives the area under the curve represented by ‘y’. Here ‘x’ is used to define the range or limits between which we want the area.
+% A = trapz (x, y)
+
+% [x = 0 : 100, will be equivalent to Xminimum = 0 &Xmaximum = 100]
+% A = cumtrapz (x, y) will compute the cumulative integration of Y wrt X. This is done using the trapezoidal integration and can be used to calculate the area under the curve for a portion.
+
+
+% plot area of AOC (strain_range)
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% PLOTTING %%%%
 % Plot orginal, interpolated, curve fits + CI, and local peaks
 figure; plot(Ringdat_SSsigep_UTSS(:,1), Ringdat_SSsigep_UTSS(:,2), 'ko','LineWidth',1.5 ); %original
@@ -145,12 +170,14 @@ hold on; findpeaks(Ringdat_SSsigep_UTSS(:,2),Ringdat_SSsigep_UTSS(:,1));
 hold on; plot(sec(:,1), sec(:,2), 'b-', 'LineWidth',1.5 ); %secant
 hold on; plot(tang_1(:,1), tang_1(:,2), 'b-', 'LineWidth',1.5 ); %tangent_1
 hold on; plot(tang_2(:,1), tang_2(:,2), 'b-', 'LineWidth',1.5 ); %tangent_2
+% plot area of AOC (strain_range)
 xlabel('Strain ε'); ylabel('Stress σ [kPa]');
-%xlim([ ]); ylim([0, 10])
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%xlim([ ]);
+ylim([ 0, max(intpdat(:,2))+max(intpdat(:,2))*0.1 ])
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Store Data
 data_cell{1,1} = 'original data'; data_cell{2,1} = Original_UTSS_Data;
 data_cell{1,2} = 'interpolated data (zero-shifted)'; data_cell{2,2} = intpdat;
@@ -162,15 +189,16 @@ data_cell{1,7} = 'delta_CI'; data_cell{2,7} = delta;
 data_cell{1,8} = strcat('secant_E[',mat2str(strain_range(2)),']'); data_cell{2,8} = secant_slope;
 data_cell{1,9} = strcat('tangent_1_E[',num2str(tangent_point_strain_1) ,']'); data_cell{2,9} = tangent_slope_1;
 data_cell{1,10} = strcat('tangent_2_E[',num2str(tangent_point_strain_2) ,']'); data_cell{2,10} = tangent_slope_2;
-data_cell{1,11} = 'yield stress'; data_cell{2,11} = Y_stress;
-data_cell{1,12} = 'yield strain'; data_cell{2,12} = Y_strain;
+data_cell{1,11} = strcat('strain energy',mat2str(strain_range)); data_cell{2,11} = AOC_over_strain_range;
+data_cell{1,12} = 'yield stress'; data_cell{2,12} = Y_stress;
+data_cell{1,13} = 'yield strain'; data_cell{2,13} = Y_strain;
 
 
-%print: p1; p2; p3, r2; secant_E; tangent_E_1; tangent_E_2; yield_stress; yield_strain
+%print: p1; p2; p3, r2; secant_E; tangent_E_1; tangent_E_2; AOC_over_strain_range; yield_stress; yield_strain
 disp([data_cell{2,4}(1); data_cell{2,4}(2); data_cell{2,4}(3);...
- data_cell{2,6}; data_cell{2,8}; data_cell{2,9}; data_cell{2,10}; data_cell{2,11}; data_cell{2,12}])
+ data_cell{2,6}; data_cell{2,8}; data_cell{2,9}; data_cell{2,10}; data_cell{2,11}; data_cell{2,12}; data_cell{2,13}])
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%  MODIFICATION IDEAS  %%%
 % Try Lo/Hi poly fit regions
 % Try basing strain_range off Yield_stress
